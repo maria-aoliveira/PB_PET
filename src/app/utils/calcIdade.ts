f9948ec17b49b8236124237569d4059aa94457aa
maria-aoliveira <maria.aoliveira@al.infnet.edu.br>
2022-11-05 09:46:43 -0300

    fix calcidade

--- src/app/utils/calcIdade.ts
+++ src/app/utils/calcIdade.ts
@@ -1,6 +1,10 @@
 import * as moment from "moment";
 
 export default function calcIdade(dataNasc: string) {
-    const momentDataNasc = moment(dataNasc);
-    return moment().diff(momentDataNasc, "years");
+    const momentDataNasc = moment(dataNasc, 'DD/MM/YYYY');
+    const todayDate = moment(new Date(), 'DD/MM/YYYY');
+    console.log(dataNasc)
+    console.log(momentDataNasc)
+    console.log( moment(todayDate).diff(momentDataNasc, "years"))
+    return moment(todayDate).diff(momentDataNasc, "years");
 } 
